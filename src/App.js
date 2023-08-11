1d1571531fd4a0c172eb6a07eeaa871f6895fc97
Lucas P.F <lucp770@gmail.com>
2023-08-11 12:41:38 -0300

    criacao do grafico de pizza estatico

--- src/App.js
+++ src/App.js
@@ -15,13 +15,20 @@ function App() {
   let dados=[{numero: 180, logo:'link', missao: 'Starlink 4-27(v1.5)', dataDeLanc: '05/10/2022', foguete:'Used Falcon 9' ,resultado:'Sucesso', video: 'link'},
   {numero: 179, logo:'link', missao: 'Starlink 4-27(v1.5)', dataDeLanc: '05/10/2022', foguete:'Used Falcon 9' ,resultado:'Sucesso', video: 'link'},
   {numero: 178, logo:'link', missao: 'Starlink 3-3(v1.5)', dataDeLanc: '07/09/2022', foguete:'Used Falcon 9' ,resultado:'Falha', video: 'link'}];
+  let chartData = [
+    ["Foguete", "nLancamentos"],
+    ["used Falcon 9", 60],
+    ["New Falcon 9", 15],
+    ["Falcon 1", 15],
+    ["Falcon Heavy", 10],
+  ];
 
   return (
     <div className="App">
 
       <h1> <img src={RocketImg} className='rocket-img'/>SPACE X </h1>
       <div className='plot-container'>
-        <PieChart data = {[1,2,3]}/>
+        <PieChart data = {chartData}/>
         <BarChart  data = {[1,2,3]}/>
       </div>
       <SearchBar handleSearch={setSearch}/>
--- src/Components/PieChart.js
+++ src/Components/PieChart.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Chart } from "react-google-charts";
+
 
 export default function pieChart({data}){
 
+    const options = {
+        title: "Lançamentos de Foguetes",
+        backgroundColor: '#abb2b9',
+      };
+
     return (
-        <div className='pie-chart'>
-            grafico de Pizza {data}
-        </div>
+        <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"450px"}
+            height={"300px"}
+        />
     )
 
 }
